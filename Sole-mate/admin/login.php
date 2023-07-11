4c510196bfb5172a58471f925032b9a1b59eb536
Ishansubedi30 <58418081+Ishansubedi30@users.noreply.github.com>
2023-07-11 15:00:34 +0545

    Initial creation of login page

---
+++ Sole-mate/admin/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="box">
+    <span class="borderline"></span>
+<form>
+<h2>Sign in</h2>
+<div class="inputBox">
+    <input type="text" required="true">
+    <span>Username</span>
+    <i></i>
+</div>
+<div class="inputBox">
+    <input type="password" required="true">
+    <span>Password</span>
+    <i></i>
+</div>
+<div class="links">
+    <a href="#">Forgot Password</a>
+    <a href="#">Signup</a>
+</div>
+<input type="submit" value="login">
+</form>
+</div>
+</body>
+</html> 
